c8215a25db1555707d9c48a032f2d8bddda59f20
Ritik <ritik09052000@gmail.com>
2021-04-22 05:27:59 +0530

    project initial setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Home from "./pages/Home";
+import Rooms from "./pages/Rooms";
+import SingleRoom from "./pages/SingleRoom";
+import Error from "./pages/Error";
+
+import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Switch>
+        {/* here exact is preventing home page to display in other pages as all pages match '/' & for other it's to prevent any future mistake (exact matches exact path mentioned */}
+        <Route exact path="/" component={Home} />
+        <Route exact path="/rooms/" component={Rooms} />
+        <Route exact path="/rooms/:slug" component={SingleRoom} />
+        {/* here ':slug' is variable which can render different pages  */}
+        <Route component={Error} />
+      </Switch>
+      {/* here switch is redirecting to error page if url does not matches any of the Route path */}
+    </>
   );
 }
 
