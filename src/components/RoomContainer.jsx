6489f37fb6a535bf1f068aaac6fde8695846327b
Ritik <ritik09052000@gmail.com>
2021-04-29 15:27:46 +0530

    modified Roomlist and roomFilter.jsx

--- src/components/RoomContainer.jsx
+++ src/components/RoomContainer.jsx
@@ -10,11 +10,10 @@ function RoomContainer({ context }) {
     return <Loading />;
   }
   return (
-    <div>
-      Hello from rooms container
+    <>
       <RoomsFilter rooms={rooms} />
       <RoomsList rooms={sortedRooms} />
-    </div>
+    </>
   );
 }
 
