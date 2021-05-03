0a0e34f1f62e84aa818278e7ec2d9ec0016c078f
Ritik <ritik09052000@gmail.com>
2021-05-03 16:10:12 +0530

    completed filters

--- src/context.js
+++ src/context.js
@@ -57,7 +57,7 @@ class RoomProvider extends Component {
 
   handleChange = (event) => {
     const target = event.target;
-    const value = event.type === "checkbox" ? target.checked : target.value;
+    const value = target.type === "checkbox" ? target.checked : target.value;
     const name = event.target.name;
 
     this.setState(
@@ -79,10 +79,42 @@ class RoomProvider extends Component {
       breakfast,
       pets,
     } = this.state;
+    // all the rooms
     let tempRooms = [...rooms];
+
+    //transform value
+    capacity = parseInt(capacity);
+    price = parseInt(price);
+
+    //room type filter
     if (type !== "all") {
       tempRooms = tempRooms.filter((room) => room.type === type);
     }
+
+    //filter by capacity of guests
+    if (capacity != 1) {
+      tempRooms = tempRooms.filter((room) => room.capacity >= capacity);
+    }
+
+    //filter by price range
+    tempRooms = tempRooms.filter((room) => room.price <= price);
+
+    //filter by room size
+    tempRooms = tempRooms.filter(
+      (room) => room.size >= minSize && room.size <= maxSize
+    );
+
+    //filter by breakfast
+    if (breakfast) {
+      tempRooms = tempRooms.filter((room) => room.breakfast === true);
+    }
+
+    //filter by pets
+    if (pets) {
+      tempRooms = tempRooms.filter((room) => room.pets === true);
+    }
+
+    // change state
     this.setState({
       sortedRooms: tempRooms,
     });
